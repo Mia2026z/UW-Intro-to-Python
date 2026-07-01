46858dfcd86715fa356558efd76f37532e7b8c33
Mia 2026z <amelia.p.zhang@icloud.com>
2026-06-30 19:12:07 -0700

    UW Intro to Python

--- hello.py
+++ hello.py
@@ -1,2 +1,32 @@
-print("Hello, World")
-print("Let's get it started")
+print("Hello, world!")
+print("My name is Amelia")
+print("My favorite hobby is dancing")
+print("My favorite number is 31")
+print("Hello\nWorld")
+print("Name:\tAmelia")
+print("She said,\"Hi!\"")
+print("It\'s fun!")
+print("C:\\Users\\Amelia")
+print("Amelia\nBlue")
+print("Food:\tapples\tDrink:\twater")
+print("My mom said:\"It\'s time to eat!\"")
+print("C:\\Python\\Projects")
+age=11
+print("I am",age,"years old")
+name="Amelia"
+age="11"
+print(f"My name is {name} and I am {age} years old")
+favnum="31"
+favhob="dance"
+hpw="6"
+print(f"I am {age} years old and my favorite number is {favnum}")
+print(f"My favorite hobby is to {favhob}, and I spend {hpw} hours per week doing it!")
+print("""My name is Amelia.
+      I love coding.
+      Python is fun!""")
+print("""There once was a girl named Lily.
+      She lived in a little house with her siblings and parents.
+      One day, she was out in the woods when a wolf saw her!
+      She was eaten and never seen again.
+      But! A kind person saved her from the wolf eventually,
+      and they all lived happily ever after!""")
