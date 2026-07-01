cd2cb97ff7583d782c818c31587914933938faa1
Mia 2026z <amelia.p.zhang@icloud.com>
2026-06-30 18:54:32 -0700

    Added line of code

--- hello.py
+++ hello.py
@@ -1,15 +1,2 @@
-print("Hello, world")
-age=12
-print("I am",age,"years old")
-name="Amelia"
-print(f"My name is {name} and I am {age} years old")
-hobby="dancing"
-favnum="31"
-print(f"My name is {name} and my favorite number is {favnum}")
-hrsperweek="6"
-print(f"My favorite hobby is {hobby} and I spend {hrsperweek} hours per week")
-print(f"My name is {name}, I am {age} years old, my favorite number is {favnum}, and I like {hobby}")
-print("""My name is Amelia.
-      I love coding.
-      Python is fun!""")
-print("My name is Amelia")
+print("Hello, World")
+print("Let's get it started")
